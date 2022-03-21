3edd2be12218755dc4c4f960e703cad5eaecabb0
Santiago Franco Mejia <santiago.franco@sofka.com.co>
2022-03-21 15:50:35 -0500

    merged structure

--- infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/CollectorMongoDBRepository.java
+++
@@ -1,8 +0,0 @@
-package co.com.bancolombia.mongo;
-
-import co.com.bancolombia.model.collector.Collector;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.query.QueryByExampleExecutor;
-
-public interface CollectorMongoDBRepository extends MongoRepository<Collector/* change for adapter model */, String> , QueryByExampleExecutor<Collector/* change for adapter model */> {
-}
--- infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/UserMongoDBRepository.java
+++
@@ -1,8 +0,0 @@
-package co.com.bancolombia.mongo;
-
-import co.com.bancolombia.model.user.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.query.QueryByExampleExecutor;
-
-public interface UserMongoDBRepository extends MongoRepository<User/* change for adapter model */, String> , QueryByExampleExecutor<User/* change for adapter model */> {
-}
